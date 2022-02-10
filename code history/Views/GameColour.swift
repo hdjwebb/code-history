b3eacf26be35d4b842d9413025ec3e77d0125580
Henry Webb <henry@ifpossible.co.uk>
2022-02-10 15:06:53 +0000

    Back to normal colours

--- code history/Views/GameColour.swift
+++ code history/Views/GameColour.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct GameColour {
-    static let main = Color(red: 40/255, green: 56/255, blue: 116/255)
-    static let accent = Color(red: 96/255, green: 210/255, blue: 120/255)
+    static let main = Color(red: 20/255, green: 28/255, blue: 58/255)
+    static let accent = Color(red: 48/255, green: 105/255, blue: 240/255)
     static let correctGuess = Color.green
     static let incorrectGuess = Color.red
 }
