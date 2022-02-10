ad7f2e11e0f36fc16cd8cd54d915e98ae0a06eac
Henry Webb <henry@ifpossible.co.uk>
2022-02-10 10:26:19 +0000

    refactor - remove old comments use new GameViewModel @stateobject update to use questionProgresstext instead or hardcoded string

--- code history/Views/GameView.swift
+++ code history/Views/GameView.swift
@@ -9,50 +9,24 @@ import SwiftUI
 
 struct GameView: View {
     
-    let question = Question(
-        questionText: "What was the first computer bug?",
-        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
-        correctAnswerIndex: 2
-    )
+//    let question = Question(
+//        questionText: "What was the first computer bug?",
+//        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
+//        correctAnswerIndex: 2
+//    )
     
-    
-    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
-//    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    @StateObject var viewModel = GameViewModel() //updated
+
     
     var body: some View {
         ZStack {
-            mainColor.ignoresSafeArea()
+            GameColour.main.ignoresSafeArea()
             VStack{
-                Text("1 / 10")
+                Text(viewModel.questionProgressText)
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
-                Text(question.questionText)
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.leading)
-                Spacer()
-                HStack{
-                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
-                        // Define the view that will be returned for each index
-                        // use the layout below
-                        //
-                        // Button(action: {
-                        //   print("Tapped on Choice 1")
-                        // }, label: {
-                        //   ChoiceTextView(choiceText: question.possibleAnswers[0])
-                        // })
-                        
-                        Button(action: {
-                          print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-                            mainColor = answerIndex == question.correctAnswerIndex ? GameColour.correctGuess : GameColour.incorrectGuess
-                        }, label: {
-                          ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
-                        })
-                    }
-
-
-                }
+                QuestionView(question: viewModel.currentQuestion)
             }
         }
         .foregroundColor(.white)
