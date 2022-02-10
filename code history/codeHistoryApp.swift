12f5de049485a1b76f45b308ec240685af4be3fd
Henry Webb <henry@ifpossible.co.uk>
2022-02-10 09:21:16 +0000

    added a line to see what happens in top file

---
+++ code history/codeHistoryApp.swift
@@ -0,0 +1,18 @@
+//
+//  code_historyApp.swift
+//  code history
+//
+//  Created by Henry Webb on 28/01/2022.
+//  adding a line here to see what happens
+//
+
+import SwiftUI
+
+@main
+struct code_historyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
