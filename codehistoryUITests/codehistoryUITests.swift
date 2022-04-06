1c439e03a5822706a78ee052cff2b7aded1196b6
Henry Webb <henrywebb@apple.com>
2022-04-06 16:07:55 +0100

    no message

--- codehistoryUITests/codehistoryUITests.swift
+++ codehistoryUITests/codehistoryUITests.swift
@@ -31,45 +31,25 @@ class codehistoryUITests: XCTestCase {
         app.windows.children(matching: .other).element.children(matching: .other).element.tap()
         app.buttons["Okay let's go!"].tap()
         
-        // Check to see if a button exists
-//        XCTAssert(app.buttons["1998"].exists, "1998 is not an option")
-//        app.buttons["1998"].tap()
-//        app.buttons["Next"].tap()
-
-//        XCTAssert(app.buttons["Steve Jobs"].exists, "Steve Jobs is not an option")
-//        XCTAssert(app.buttons["Python"].exists, "Python is not an option")
-//        XCTAssert(app.buttons["Everest"].exists, "1998 is not an option")
-//        XCTAssert(app.buttons["Pepino"].exists, "Pepino is not an option")
-//        XCTAssert(app.buttons["5"].exists, "5 is not an option")
-//        XCTAssert(app.buttons["14"].exists, "5 is not an option")
-        for _ in 1...7 {
-            if app.buttons["1998"].exists {
-                app.buttons["1998"].tap()
+        for _ in 1...5 {
+            if app.buttons["1"].exists {
+                app.buttons["1"].tap()
                 app.buttons["Next"].tap()
             }
-            if app.buttons["Simula"].exists {
-                app.buttons["Simula"].tap()
+            if app.buttons["2"].exists {
+                app.buttons["2"].tap()
                 app.buttons["Next"].tap()
             }
-            if app.buttons["Tim Berners-Lee"].exists {
-                app.buttons["Tim Berners-Lee"].tap()
+            if app.buttons["3"].exists {
+                app.buttons["3"].tap()
                 app.buttons["Next"].tap()
             }
-            if app.buttons["Everest"].exists {
-                app.buttons["Everest"].tap()
-                app.buttons["Next"].tap()
-            }
-            if app.buttons["Pepino"].exists {
-                app.buttons["Pepino"].tap()
+            if app.buttons["4"].exists {
+                app.buttons["4"].tap()
                 app.buttons["Next"].tap()
             }
             if app.buttons["5"].exists {
                 app.buttons["5"].tap()
-                app.buttons["Next"].tap()
-                
-            }
-            if app.buttons["14"].exists {
-                app.buttons["14"].tap()
                 app.buttons["Next"].tap()
             }
         }
